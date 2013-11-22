0a2965b3b514379dc7a1de70036f97a2f6d526a6
Navié OS <info@dpit2.de>
2013-11-22 21:41:37 +0100

    c4d_symbols

---
+++ c4d_symbols.h
@@ -0,0 +1,32 @@
+enum
+{
+	_FIRST_ELEMENT_      = 10000,
+  // Dialog definitions of ID_RAMPLAYER start here
+  ID_RAMPLAYER,
+  IDC_TOPMOST,
+  RP_PATH,
+  RP_G_TIMELINE,
+  RP_TOSTART,
+  RP_ONEBACK,
+  RP_REWIND,
+  RP_STOP,
+  RP_PLAY,
+  RP_ONEFOR,
+  RP_TOEND,
+  RP_FPS,
+  RP_SIZE,
+  RP_G_SETTINGS,
+  RP_FRAME,
+  RP_G_DISPLAY,
+  RP_MOVIE,
+  // Dialog definitions of ID_RAMPLAYER end here
+
+	RP_MENU_FILE,
+	RP_MENU_LOAD,
+	RP_MENU_SAVE_QT,
+	RP_MENU_MERGE,
+	RP_MENU_SAVE_AVI,
+
+// End of symbol definition
+  _DUMMY_ELEMENT_
+};
